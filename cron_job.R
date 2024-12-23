15be376e58c644a8024ef1b2fba848ad7608af14
ThierryUdeM <84813356+ThierryUdeM@users.noreply.github.com>
2024-12-22 20:15:50 -0500

    update

---
+++ cron_job.R
@@ -0,0 +1,21 @@
+library(AzureStor)
+library(readr)
+
+storage_account_name <- Sys.getenv("AZURE_STORAGE_ACCOUNT")
+container_name <- Sys.getenv("AZURE_CONTAINER_NAME")
+access_key <- Sys.getenv("AZURE_ACCESS_KEY")
+
+# Create a blob endpoint
+endpoint <- storage_endpoint(
+  paste0("https://", storage_account_name, ".blob.core.windows.net"),
+  key=access_key
+)
+
+# Connect to the container
+container <- blob_container(endpoint, container_name)
+
+#2. Télécharger données brutes companies
+data <- storage_read_csv(container, 'ecosense.csv')
+saveRDS(data, "file.rds")
+
+upload_blob(container, "file.rds", dest = "ecosense.rds")
